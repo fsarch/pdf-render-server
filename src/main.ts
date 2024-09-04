a077d234c1dd4e552dfc1b93b3f6f4bd9b140be6
Michael Braun <michael@web4brauns.de>
2024-09-04 18:40:51 +0200

    Increase json body limit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import express from 'express';
 import { AppModule } from './app.module.js';
+
+const PORT = process.env.PORT ? (parseInt(process.env.PORT) || 3000) : 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(express.json({limit: '50mb'}));
+  await app.listen(PORT);
 }
 bootstrap();
