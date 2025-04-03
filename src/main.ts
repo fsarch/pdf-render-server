b774d7ef24b9da0e2cd1bec092f072281926acc0
Michael Braun <michael@web4brauns.de>
2025-04-03 22:41:57 +0200

    Update nestjs and add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import express from 'express';
 import { AppModule } from './app.module.js';
-
-const PORT = process.env.PORT ? (parseInt(process.env.PORT) || 3000) : 3000;
+import { VersioningType } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Material-Tracing-Server')
+    .setDescription('The Material-Tracing-Server API description')
+    .addBearerAuth()
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   app.use(express.json({limit: '50mb'}));
-  await app.listen(PORT);
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
